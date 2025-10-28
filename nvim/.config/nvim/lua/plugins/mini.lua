e6becba3ffedeb8fd4d6f16d4a6b8391c3b2e4bd
lenymrq <lenymrq@gmail.com>
2025-10-28 13:29:55 +0100

    nvim: change colorscheme

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -34,27 +34,7 @@ return {
         },
       }
 
-      require('mini.base16').setup {
-        -- gruvbox dark hard
-        palette = {
-          base00 = '#1d2021',
-          base01 = '#3c3836',
-          base02 = '#504945',
-          base03 = '#665c54',
-          base04 = '#bdae93',
-          base05 = '#d5c4a1',
-          base06 = '#ebdbb2',
-          base07 = '#fbf1c7',
-          base08 = '#fb4934',
-          base09 = '#fe8019',
-          base0A = '#fabd2f',
-          base0B = '#b8bb26',
-          base0C = '#8ec07c',
-          base0D = '#83a598',
-          base0E = '#d3869b',
-          base0F = '#d65d0e',
-        },
-      }
+      require('mini.hues').setup { background = '#000000', foreground = '#ffffff', saturation = 'high', accent = 'azure' }
 
       require('mini.icons').setup()
       MiniIcons.tweak_lsp_kind()
