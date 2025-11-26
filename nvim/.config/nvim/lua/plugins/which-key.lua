9765d6700744f8f58c9ee0b02f158b468ab79700
lenymrq <lenymrq@gmail.com>
2025-11-26 14:18:29 +0100

    nvim: which key add keymaps descriptions and groups

--- nvim/.config/nvim/lua/plugins/which-key.lua
+++ nvim/.config/nvim/lua/plugins/which-key.lua
@@ -4,16 +4,28 @@ return {
     opts = {
       preset = 'helix',
       delay = 0,
-    },
-    init = function()
-      local which_key = require 'which-key'
-      which_key.add {
+      spec = {
         { '<leader>b', group = '[B]uffer' },
         { '<leader>e', group = '[E]xplore' },
         { '<leader>o', group = '[O]ther' },
-        { '<leader>s', group = '[S]urround' },
+        {
+          '<leader>s',
+          group = '[S]urround',
+          { '<leader>sa', desc = '[A]dd [S]urrounding' },
+          { '<leader>sd', desc = '[D]elete [S]urrounding' },
+          { '<leader>sf', desc = '[F]ind [S]urrounding' },
+          { '<leader>sF', desc = '[F]ind [S]urrounding Left' },
+          { '<leader>sh', desc = '[H]ighlight [S]urrounding' },
+          { '<leader>sr', desc = '[R]eplace [S]urrounding' },
+        },
         { '<leader>t', group = '[T]elescope' },
-      }
-    end,
+        {
+          'gr',
+          group = 'LSP',
+          { 'gra', desc = 'LSP Code [A]ctions' },
+          { 'grn', desc = 'LSP Re[n]ame' },
+        },
+      },
+    },
   },
 }
