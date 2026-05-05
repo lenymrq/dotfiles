644028d33efe9acf66321b003a4a95d1cc0613ca
lenymrq <lenymrq@gmail.com>
2026-05-05 16:11:25 +0200

    zsh: enhance git prompt

--- zsh/.zsh/git-prompt.zsh
+++ zsh/.zsh/git-prompt.zsh
@@ -2,6 +2,18 @@ autoload -Uz vcs_info
 
 zstyle ':vcs_info:*' enable git
 zstyle ':vcs_info:git:*' check-for-changes true
+zstyle ':vcs_info:git:*' get-unapplied true
+zstyle ':vcs_info:*' formats '[%F{blue}%b%f]-%F{red}%u%f%F{green}%c%f'
+zstyle ':vcs_info:*' actionformats '[%F{blue}%b%f|%F{yellow}%a%f]-%F{red}%u%f%F{green}%c%f'
+
+# Display untracked files
+zstyle ':vcs_info:git*+set-message:*' hooks git-untracked
++vi-git-untracked(){
+    if [[ $(git rev-parse --is-inside-work-tree 2> /dev/null) == 'true' ]] && \
+        git status --porcelain | grep -q '^?? ' 2> /dev/null ; then
+        hook_com[unstaged]+='T'
+    fi
+}
 
 function _git_prompt () {
     vcs_info
